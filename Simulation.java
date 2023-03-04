dc02666755af98e5969aff9dde8e0c400d40e03e
catherinelin <clin1@brynmawr.edu>
2023-03-03 19:21:12 -0500

    simulation stuff

--- Simulation.java
+++ Simulation.java
@@ -1,15 +1,77 @@
+import java.util.HashMap;
+import java.util.Random;
 public class Simulation {
+    private static final double minStartingInterval = 0.1; // in seconds; should be at least 0.1
+    private static final double startingIntervalVar = 2.0; 
+    private static final int NUM_COURSES=5;
+    private static final int DESIRED=2;
+    private static final int OKAY=2;
 
     public static void runSequentialExample() {
-        System.out.println("== Starting sequential trucks example ==");
-        
+        System.out.println("== Starting sequential example ==");
+
         System.out.println("== Sequential example is done now.");
 
     }
 
     public static void runConcurrentExample() {
+      
         
-    }
+
+        System.out.println("== Starting concurrent trucks example. ==");
+		Registrar R = new Registrar(NUM_COURSES);
+        for(int i=0;i<NUM_COURSES;i++) {
+            R.makeNewCourse(2,i);
+        }
+
+		HashMap<Integer, Integer> desired=new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> okay=new HashMap<Integer, Integer>();
+        for(int i=0;i<DESIRED;i++) {
+            desired.put(i,i);
+        }
+        for(int i=DESIRED;i<NUM_COURSES;i++) {
+            okay.put(i-DESIRED,i);
+        }
+       
+        Student T= new Student(0,R,desired,okay);
+		Student U = new Student(1,R,desired,okay);
+		T.start(); tryToSleep(minStartingInterval, startingIntervalVar);
+		U.start(); tryToSleep(minStartingInterval, startingIntervalVar);
+		/*
+
+
+		Truck V = new Truck("V",  80, 2, B);
+		Truck W = new Truck("W", 200, 3, B);
+		Truck X = new Truck("X", 100, 1, B);
+		Truck Y = new Truck("Y",  80, 1, B);
+		Truck Z = new Truck("Z", 100, 1, B);
+
+
+		V.start(); tryToSleep(minStartingInterval, startingIntervalVar);
+		W.start(); tryToSleep(minStartingInterval, startingIntervalVar);
+		X.start(); tryToSleep(minStartingInterval, startingIntervalVar);
+		Y.start(); tryToSleep(minStartingInterval, startingIntervalVar);
+		Z.start(); tryToSleep(minStartingInterval, startingIntervalVar);
+
+		 */
+		
+		try {
+			T.join();  // wait for T to be done
+			U.join();
+			/*
+			V.join();
+			W.join();
+			X.join();
+			Y.join();
+			Z.join();
+			 */
+		} catch  (Exception e) {
+            System.out.println("Not Handling exceptions yet ... goodbye");
+        }
+        System.out.println("== Concurrent trucks example is done now. ==");
+	}
+        
+    
 
     private static final java.util.Random dice = new java.util.Random(); // random number generator, for delays mostly
 	public static void tryToSleep(double minimumSeconds, double upToMoreSeconds) {
--- Student.java
+++ Student.java
@@ -7,9 +7,9 @@ public class Student extends Thread {
     HashMap<Integer, Integer> desired;
     HashMap<Integer, Integer> okay;
     private final Registrar registrar;
-    private final int COURSE_NUM=4;
-    private final int ALSO_OKAY=8;
-    private final int MAX_TRIES=25;
+    private int COURSE_NUM;
+    private int ALSO_OKAY;
+    private final int MAX_TRIES=5;
     private final long TIME_TO_WAIT=200;
     PriorityQueue<StudentCourse> student_courses;
     int tries=0;
@@ -19,6 +19,8 @@ public class Student extends Thread {
         this.student_id=student_id;
         this.registrar=registrar;
         this.desired=desired;
+        COURSE_NUM=desired.size();
+        ALSO_OKAY=okay.size();
         student_courses=new PriorityQueue<StudentCourse>();
         for(int i=0;i<4;i++) {
             StudentCourse dummy=new StudentCourse(-i, -1);
@@ -37,13 +39,21 @@ public class Student extends Thread {
     }
 
     void addCourse() {
+
+        removeCourse();
+
+        //choose course to add
         int desiredCourse;
         if(student_courses.peek().desire_rank()==-1) {
-            desiredCourse=okay.get((int)(Math.random()*ALSO_OKAY));
+            desiredCourse=okay.get(tries%ALSO_OKAY);
+            //desiredCourse=okay.get((int)(Math.random()*ALSO_OKAY));
         }
         else {
-            desiredCourse=desired.get((int)(Math.random()*COURSE_NUM));
+            desiredCourse=desired.get(tries%COURSE_NUM);
+            //desiredCourse=desired.get((int)(Math.random()*COURSE_NUM));
         }
+
+        //add whichever course the registrar allows
         int courseToAdd=registrar.addCourse(student_id,desiredCourse);
 
         if(desired.containsValue(courseToAdd)) {
@@ -69,8 +79,6 @@ public class Student extends Thread {
 
         }while(!removedFromCourse);
         
-
-        //remove course
         student_courses.poll();
          
         assert numCourses()==COURSE_NUM-1;
@@ -90,7 +98,13 @@ public class Student extends Thread {
     */
 
 
+    void printSchedule() {
+        for(StudentCourse sc:student_courses) {
+            System.out.println("CourseID: "+sc.course_id());
 
+        }
+      
+    }
     public void run() {
         while(success<COURSE_NUM && tries<MAX_TRIES) {
             addCourse();
@@ -104,9 +118,11 @@ public class Student extends Thread {
         }
         if(success==COURSE_NUM) {
             System.out.println(":)");
+            printSchedule();
         }
         else {
             System.out.println(":(");
+            printSchedule();
         }
     }
 
